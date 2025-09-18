d76a255071b869422e772261e436a262f5fdd5cb
robertt-dev <roberttmehary.dev@gmail.com>
2025-09-18 16:33:31 -0300

    logica-console-pronto

---
+++ src/main/java/Aplicacao.java
@@ -0,0 +1,15 @@
+import Modelo.Tabuleiro;
+
+public class Aplicacao {
+
+    public static void main(String[] args) {
+
+        Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
+
+        tabuleiro.abrir(3, 3);
+        tabuleiro.alternarMarcacao(4, 4);
+        tabuleiro.alternarMarcacao(4, 5);
+
+        System.out.println(tabuleiro);
+    }
+}
